3d7d65dc07172f393819cd041ffef1b49ca6c2ff
“Kevin” <yuehcw@uci.edu>
2024-01-14 19:29:12 -0800

    reupdate

--- routes/authors.js
+++ routes/authors.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const Author = require("../models/author")
 
 
-// All Authors Route
+// All Authors Routes
 router.get('/', async(req, res) => {
     let searchOptions = {}
     if(req.query.name != null && req.query.name !== '') {
